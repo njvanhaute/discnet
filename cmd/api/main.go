4b998efdc366221228bfa74fe398d8a736bc8d3d
Nicholas Vanhaute <nvanhaute@wisc.edu>
2024-10-03 20:32:46 -0500

    Service skeleton
    Add panic recovery middleware, some HTTP error responses, a healthcheck
    route, and some other barebones server functionality.

---
+++ cmd/api/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"flag"
+	"log/slog"
+	"os"
+	"sync"
+
+	"github.com/njvanhaute/discnet/internal/vcs"
+)
+
+type config struct {
+	port int
+	env  string
+}
+type application struct {
+	config config
+	logger *slog.Logger
+	wg     sync.WaitGroup
+}
+
+var (
+	version = vcs.Version()
+)
+
+func main() {
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	err := app.serve()
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+}
